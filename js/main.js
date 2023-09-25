bcd845bd281d5982c1c70ac3ef61e2d5e703a44b
F.Chabrou <francois.chabrou@gmail.com>
2023-09-25 18:57:58 +0200

    delete API key

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+const API_KEY = 'secret'
 
 const testReading = () => {
-  fetch('https://sheets.googleapis.com/v4/spreadsheets/1L4mulEYdrYsD6xZ20NgS7vzxD7tK29joijHrVRK04JI/values/main!A1:A3?key=AIzaSyBce_WJ3Tm-OxbXJauBx-JOhJ93XjgrH38')
+  fetch(`https://sheets.googleapis.com/v4/spreadsheets/${API_KEY}/values/main!A1:A3?key=AIzaSyBce_WJ3Tm-OxbXJauBx-JOhJ93XjgrH38`)
     .then(res => {
       res.json().then(json => console.log('res', json))
     })
