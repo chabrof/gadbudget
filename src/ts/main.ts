3569a1da28cd3df881d2fefe853ea72a11b24575
F.Chabrou <francois.chabrou@gmail.com>
2023-10-07 15:30:21 +0200

    test google api

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,29 @@
-const API_KEY = 'secret'
-
+const API_KEY = 'AIzaSyBce_WJ3Tm-OxbXJauBx-JOhJ93XjgrH38'
+const spreadsheetId = '1L4mulEYdrYsD6xZ20NgS7vzxD7tK29joijHrVRK04JI'
 const testReading = () => {
-  fetch(`https://sheets.googleapis.com/v4/spreadsheets/${API_KEY}/values/main!A1:A3?key=AIzaSyBce_WJ3Tm-OxbXJauBx-JOhJ93XjgrH38`)
+  fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/main!A1:A3?key=${API_KEY}`)
     .then(res => {
       res.json().then(json => console.log('res', json))
     })
 }
 
-testReading()
+// testReading()
+
+const testWriting = () => {
+  /*fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/main!A1:A3:append?key=${API_KEY}&valueInputOption=USER_ENTERED&insertDataOption=OVERWRITE`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: {
+      majorDimension: "ROWS",
+      range:"main!A1:A3",
+      values:['Test ecriture']
+    }
+  })
+  .then(res => {
+    res.json().then(json => console.log('res', json))
+  })*/
+}
+
+testWriting()
